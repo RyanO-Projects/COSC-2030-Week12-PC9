9a964e184378a9ff6eb0a91af3b05edbcee22170
Ryan <ryan.opitz@mycc.caspercollege.edu>
2023-11-21 20:50:12 -0700

    Created file reverser
    "words.txt" was created to store the text that is to be read in. "copy.txt" was created to store the output of the reversed text.

--- FileReverser.cpp
+++ FileReverser.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <stack>
+#include <vector>
 
 
 int main() {
     std::ifstream inFile;
+    std::ofstream outFile;
     std::string filename;
+    std::stack<char, std::vector<char>> istack;
     
-    std::cout << "Enter the file name:\n";
+    std::cout << "Enter the file name to read from:\n";
     std::cin >> filename;
     inFile.open(filename);
 
@@ -16,5 +19,19 @@ int main() {
         return 0;
     }
 
-    
+    std::cout << "Enter the file name to store to:\n";
+    std::cin >> filename;
+    outFile.open(filename);
+
+    char ch;
+    while(inFile.get(ch)) {
+        istack.push(ch);
+    }
+
+    while(!istack.empty()) {
+        outFile << istack.top();
+        istack.pop();
+    }
+    inFile.close();
+    outFile.close();
 }
